6eea50971dd0bf75869fdc5ba72c5eb045ee98fe
udany <long.eagle.goodies@gmail.com>
2018-04-01 13:54:19 -0300

    Index mock

--- album-tracker/pages/index.vue
+++ album-tracker/pages/index.vue
@@ -1,27 +1,52 @@
 <template>
-    <div class="container mt-4">
+    <div class="container my-4">
         <b-row>
             <b-col>
-                <h1>
+                <h1 class="my-4">
                     Welcome to Album Tracker!
                 </h1>
             </b-col>
         </b-row>
 
         <b-row>
-            <b-col md="3" class="mt-4">
-                <square>
-                    <h2 class="ml-4 mt-4">Test</h2>
-                </square>
+            <b-col>
+                <h2 class="m-0">Popular Collections</h2>
             </b-col>
-            <b-col md="3" class="mt-4">
-                <square></square>
+        </b-row>
+
+        <b-row>
+            <b-col md="3" cols="6" class="mt-4">
+                <square-cover :id="0" icon="chevron-right" title="Chill mix"></square-cover>
             </b-col>
-            <b-col md="3" class="mt-4">
-                <square></square>
+            <b-col md="3" cols="6" class="mt-4">
+                <square-cover :id="1" icon="chevron-right" title="Cozy by the fire"></square-cover>
             </b-col>
-            <b-col md="3" class="mt-4">
-                <square></square>
+            <b-col md="3" cols="6" class="mt-4">
+                <square-cover :id="2" icon="chevron-right" title="Party Hard"></square-cover>
+            </b-col>
+            <b-col md="3" cols="6" class="mt-4">
+                <square-cover :id="3" icon="chevron-right" title="Countryside harkens"></square-cover>
+            </b-col>
+        </b-row>
+
+        <b-row>
+            <b-col>
+                <h2 class="mb-0 mt-4">Popular Albums</h2>
+            </b-col>
+        </b-row>
+
+        <b-row>
+            <b-col md="3" cols="6" class="mt-4">
+                <square-cover :id="0" icon="chevron-right" title="Chill mix"></square-cover>
+            </b-col>
+            <b-col md="3" cols="6" class="mt-4">
+                <square-cover :id="1" icon="chevron-right" title="Cozy by the fire"></square-cover>
+            </b-col>
+            <b-col md="3" cols="6" class="mt-4">
+                <square-cover :id="2" icon="chevron-right" title="Party Hard"></square-cover>
+            </b-col>
+            <b-col md="3" cols="6" class="mt-4">
+                <square-cover :id="3" icon="chevron-right" title="Countryside harkens"></square-cover>
             </b-col>
         </b-row>
     </div>
@@ -29,12 +54,12 @@
 
 <script>
     import AppLogo from '~/components/AppLogo.vue'
-    import Square from '~/components/Square.vue'
+    import SquareCover from '~/components/SquareCover.vue'
 
     export default {
         components: {
             AppLogo,
-            Square
+            SquareCover
         }
     }
 </script>
