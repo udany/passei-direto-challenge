68b8242df1741d340b55e172e3d700840416858b
udany <long.eagle.goodies@gmail.com>
2018-04-01 07:30:45 -0300

    Square Component - Early work on the album Cover component

---
+++ album-tracker/components/Square.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="square">
+        <div class="content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<style>
+    .square{
+        width: 100%;
+        background: #2b3474;
+        position: relative;
+    }
+    .square:before{
+        content:'';
+        padding:50% 0; /* vertical value as  100% equals width */
+        display:inline-block;
+    }
+    .square .content {
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+    }
+</style>
--- album-tracker/pages/index.vue
+++ album-tracker/pages/index.vue
@@ -7,15 +7,34 @@
                 </h1>
             </b-col>
         </b-row>
+
+        <b-row>
+            <b-col md="3" class="mt-4">
+                <square>
+                    <h2 class="ml-4 mt-4">Test</h2>
+                </square>
+            </b-col>
+            <b-col md="3" class="mt-4">
+                <square></square>
+            </b-col>
+            <b-col md="3" class="mt-4">
+                <square></square>
+            </b-col>
+            <b-col md="3" class="mt-4">
+                <square></square>
+            </b-col>
+        </b-row>
     </div>
 </template>
 
 <script>
     import AppLogo from '~/components/AppLogo.vue'
+    import Square from '~/components/Square.vue'
 
     export default {
         components: {
-            AppLogo
+            AppLogo,
+            Square
         }
     }
 </script>
